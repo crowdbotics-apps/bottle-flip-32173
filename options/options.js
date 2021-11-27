9afdd43adea993cf59c0b6e8e39808ffd11d97ff
Crowdbotics <team@crowdbotics.com>
2021-11-27 04:34:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bottle_flip_32173",
+  url: "https://bottle_flip_32173.botics.co",
+  api: "https://bottle_flip_32173.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
